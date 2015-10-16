49a7712daf737e4b03b0a1bfb00c12a4856af346
ylagorebollar <ylagorebollar@atanasoff04.danielcastelao.org>
2015-10-16 12:14:54 +0200

    cambios

--- src/exemplo4/Exemplo4.java
+++ src/exemplo4/Exemplo4.java
@@ -4,15 +4,18 @@ import javax.swing.JOptionPane;
 
 public class Exemplo4 {
     public static void main(String[] args) {
-     /*Rectangulo rec1=new Rectangulo(3,5);
+     /*Rectangulo rec1=new Rectangulo(3,5);//Creación do obxeto rec1
      System.out.println("base"+rec1.getBase());
      System.out.println("altura"+rec1.getAltura());
      System.out.println("area"+rec1.calcularArea(7, 8));
      System.out.println("perimetro"+rec1.calcularPerimetro());*/
-     float base,altura;
+    /* float base,altura;
      String resposta=JOptionPane.showInputDialog("base");
      base=Float.parseFloat(resposta);
-     altura=Float.parseFloat(JOptionPane.showInputDialog("altura"));//Simplificación de código
+     altura=Float.parseFloat(JOptionPane.showInputDialog("altura"));//Simplificación de código*/
+     Metodos aux=new Metodos();
+     float base=aux.pedirFloat();
+     float altura=aux.pedirFloat();
      Rectangulo rec2=new Rectangulo(base,altura);
      JOptionPane.showMessageDialog(null,"Area"+rec2.calcularArea(base,altura));
      JOptionPane.showMessageDialog(null,"perimetro"+rec2.calcularPerimetro());
